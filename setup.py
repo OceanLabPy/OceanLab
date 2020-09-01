4681d2fbca3ec5d6298fd5635d248ed2781f9c81
iury simoes-sousa <simoesiury@gmail.com>
2020-09-01 16:55:16 -0400

    install_requirements

--- setup.py
+++ setup.py
@@ -1,18 +1,30 @@
 from setuptools import setup
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
+
+with open('README.md','r') as fh:
+    long_description = fh.read()
 
 setup(
     name='OceanLab',
     version='0.0.1',
     description='Python functions for Physical Oceanography',
-    long_description=readme(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/iuryt/OceanLab',
     author='Iury T. Simoes-Sousa',
     author_email='simoesiury@gmail.com',
     license='MIT',
     py_modules=['OA','EOF','DYN'],
     package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: MIT",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = [
+        'seawater ~= 3.3',
+        'numpy ~= 1.18',
+    ],
 )
